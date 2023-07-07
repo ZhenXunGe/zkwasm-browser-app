24a25daf0907fcfe10f305175f1c30d4c4debd72
rhaoio <richard@rhao.io>
2023-07-08 00:20:56 +1000

    add instance images

--- src/components/ChangeInstance/index.tsx
+++ src/components/ChangeInstance/index.tsx
@@ -1,5 +1,5 @@
 import { Modal } from "react-bootstrap";
-
+import "./style.scss";
 interface ChangeInstanceProps {
   show: boolean;
   handleClose: () => void;
@@ -9,8 +9,24 @@ interface ChangeInstanceProps {
 export default function Events(props: ChangeInstanceProps) {
   return (
     <>
-      <Modal show={props.show} onHide={props.handleClose}>
-        <Modal.Header closeButton>Change Map</Modal.Header>
+      <Modal
+        show={props.show}
+        onHide={props.handleClose}
+        className="game-dialog"
+      >
+        <div className="close-bag"></div>
+        <div className="instance-body"></div>
+        <div className="x-marker" style={{ left: "33%", top: "35%" }}></div>
+        <div className="x-marker" style={{ left: "33%", top: "50%" }}></div>
+        <div className="x-marker" style={{ left: "63%", top: "38%" }}></div>
+        <div className="x-marker" style={{ left: "45%", top: "60%" }}></div>
+        <div className="x-marker" style={{ left: "63%", top: "75%" }}></div>
+        <div
+          className="hitbox"
+          onClick={() => {
+            props.handleClose();
+          }}
+        ></div>
       </Modal>
     </>
   );
